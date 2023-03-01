40bf651786fc0aaaae3d2c9bcf45fa890e6db309
TerenceBruce <98389495+TerenceBruce@users.noreply.github.com>
2023-03-01 14:12:02 +0000

    stripe integration
    stripe integration still needs work

--- reactapp/functions/index.js
+++ reactapp/functions/index.js
@@ -1,10 +1,10 @@
     const functions = require("firebase-functions");
-    exports.createStripeCheckout= functions.https.onCall((data,context) => {
+    // exports.createStripeCheckout= functions.https.onCall((data,context) => {
       const stripe = require('stripe')(functions.config().stripe.secret.key);
       const express = require('express');
       const app = express();
       app.use(express.static('public'));
-      
+      // reactapp-d41d0
       const YOUR_DOMAIN = 'http://localhost:3000';
       
       app.post('/create-checkout-session', async (req, res) => {
@@ -12,7 +12,8 @@
           line_items: [
             {
               // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
-              price: '{{PRICE_ID}}',
+              price: '',
+              quantity: 1,
             },
           ],
           mode: 'payment',
@@ -24,7 +25,7 @@
         res.redirect(303, session.url);
       });
       
-      app.listen(4242, () => console.log('Running on port 4242'));
+    //   app.listen(4242, () => console.log('Running on port 4242'));
 
-    })
+    // })
     
--- reactapp/src/components/ViewBasket.jsx
+++ reactapp/src/components/ViewBasket.jsx
@@ -42,7 +42,9 @@ export default function ViewBasket() {
                   thousandSeparator={true}
                   prefix={"£"}
                 />
-   
+          <form action="/create-checkout-session" method="post">
+            <button type="submit" id="checkout-button" class="btn btn-primary">Checkout</button>
+            </form>
           </Modal.Body>
           <Modal.Footer>
             <button variant="secondary" onClick={handleClose}>
