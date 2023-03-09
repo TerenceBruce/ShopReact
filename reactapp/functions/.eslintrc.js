8b6039369314cae30866ab11628f2fe00958f902
TerenceBruce <98389495+TerenceBruce@users.noreply.github.com>
2023-03-09 11:47:16 +0000

    fucntions blaze plan not working

--- reactapp/functions/.eslintrc.js
+++ reactapp/functions/.eslintrc.js
@@ -6,7 +6,7 @@ module.exports = {
   },
   extends: [
     "eslint:recommended",
-    "google",
+    // "google",
   ],
   rules: {
     quotes: ["error", "double"],
---
+++ reactapp/functions/index.js
@@ -0,0 +1,7 @@
+// const { response } = require("express");
+const functions = require("firebase-functions");
+
+exports.helloWorld = functions.https.onRequest((request, response) => {
+    functions.logger.info("Hello logs!",{structuredData: true});
+    response.send("Hello from Firebase!");
+})
