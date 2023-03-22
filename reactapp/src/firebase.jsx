316ea3356b61e71bb982091275f09ab2e0ba1d62
TerenceBruce <98389495+TerenceBruce@users.noreply.github.com>
2023-03-22 13:16:52 +0000

    fixed env file
    the env is done now secret ;)

--- reactapp/src/firebase.jsx
+++ reactapp/src/firebase.jsx
@@ -5,14 +5,20 @@ import { getStorage} from "firebase/storage";
 
 // update to env.local
 const app  = initializeApp({
-  apiKey: "AIzaSyD-R1DjXGmk2Khn08syxOvrk5NgkYM4ooc",
-  authDomain: "reactapp-d41d0.firebaseapp.com",
-  projectId: "reactapp-d41d0",
-  storageBucket: "reactapp-d41d0.appspot.com",
-  messagingSenderId: "1070373665336",
-  appId: "1:1070373665336:web:64f02b99679459a58744cd",
-  measurementId: "G-KDLS6KFSBW"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 })
+
+
+
+
+
+
 
 
 export const auth = getAuth(app)
--- reactapp/src/pages/Home.jsx
+++ reactapp/src/pages/Home.jsx
@@ -1,26 +1,17 @@
 import {React} from 'react'
-import { Button } from 'react-bootstrap';
+import { Card } from 'react-bootstrap';
 // import ReadStorage from './ReadStorage'
 // import TestProduct from './TestProduct'
-
-import {  Link } from "react-router-dom";
-
+import ProductList from '../components/ProductList'
 import '../css/home.css';
-
 export default function Home() {
     
   return (
+    
     <div className="container">
-      <div className="row">
-        <div className="col">Still Fresh</div>
-        <div className="col">
-          <Link to="/Shop">Enter Shop</Link>
-        </div>
-        <div className="col">
-          <button>About </button>
-        </div>
       
-      </div>
     </div>
+  
+    
   );
 }
