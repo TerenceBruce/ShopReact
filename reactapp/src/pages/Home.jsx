8d87d364909e2f346d97f5f11c1404c8d7213d66
J-R-BIThub <j.r.bit@outlook.com>
2023-05-01 22:57:30 +0100

    Updated NavBar component and started using throughout
    Updated NavBar component and started using throughout. Want to try find a way of creating 'master pages'

--- reactapp/src/pages/Home.jsx
+++ reactapp/src/pages/Home.jsx
@@ -1,17 +1,15 @@
 import {React} from 'react'
 
 import {  Link } from "react-router-dom";
+import NavigationBar from '../components/NavigationBar';
 import Footer from '../components/Footer';
 
 //Bootstrap...elements individualy to avoid installing full BS packages
 import Button  from 'react-bootstrap/Button'; 
 import "bootstrap/dist/css/bootstrap.min.css";
 import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
 import Image from 'react-bootstrap/Image';
 import Card from 'react-bootstrap/Card';
 // import ReadStorage from './ReadStorage'
@@ -23,29 +21,7 @@ export default function Home() {
   return (
     <body>
       <header>
-        <Navbar bg="dark" variant="dark" expand="lg">
-          <Container>
-            <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-              <Nav className="me-auto">
-                <Nav.Link href="#home">Home</Nav.Link>
-                <Nav.Link href="#link">Link</Nav.Link>
-                <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                  <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                  <NavDropdown.Item href="#action/3.2">
-                    Another action
-                  </NavDropdown.Item>
-                  <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                  <NavDropdown.Divider />
-                  <NavDropdown.Item href="#action/3.4">
-                    Separated link
-                  </NavDropdown.Item>
-                </NavDropdown>
-              </Nav>
-            </Navbar.Collapse>
-          </Container>
-        </Navbar>
+        <NavigationBar/>
       </header>
       <main>
         <Container>
@@ -109,7 +85,7 @@ export default function Home() {
           </Row>
         </Container>
       </main>
-      
+
       <Footer/>
     </body>
   );
