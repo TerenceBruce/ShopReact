7ed22aaeed1ab5b1fd5469b6ce70a3b2195e5279
TerenceBruce <98389495+TerenceBruce@users.noreply.github.com>
2023-05-10 14:08:09 +0100

    undo

--- reactapp/src/pages/ShopPage.jsx
+++ reactapp/src/pages/ShopPage.jsx
@@ -26,33 +26,7 @@ export default function ShopPage() {
             </div>
             <ProductList />
             <div className="col">
-              <div className="box">3</div>
-            </div>
-            <div className="col">
-              <div className="box">4</div>
-            </div>
-            <div className="col">
-              <div className="box">5</div>
-            </div>
-          </div>
-
-          <div className="row">
-            <div className="col">
-              <div className="box">6</div>
-            </div>
-
-            <div className="col ">
-              <div className="box">7</div>
-            </div>
-
-            <div className="col">
-              <div className="box">8</div>
-            </div>
-            <div className="col">
-              <div className="box">9</div>
-            </div>
-            <div className="col">
-              <div className="box">10</div>
+              <div className="box">2</div>
             </div>
           </div>
         </Container>
